deae8402ed9b807158df6748f4b22b310c598329
swain1467 <swain1467@gmail.com>
2023-10-18 18:12:43 +0530

    completed queue redis

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // sleep(5);
+    // \Mail::to('swain1467@gmail.com')->send(new \App\Mail\SendTestMail());
+    dispatch(new \App\Jobs\SendEmailJob());
     return view('welcome');
 });
 
